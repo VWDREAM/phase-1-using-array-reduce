219cb3fe9a8124b020cfac1b29076a619221ec61
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-03-13 19:37:46 +0300

    array reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+const totalBatteries = batteryBatches.reduce((sum, battery) => sum + battery, 0);
+
+module.exports = { totalBatteries }; // Export for testing
